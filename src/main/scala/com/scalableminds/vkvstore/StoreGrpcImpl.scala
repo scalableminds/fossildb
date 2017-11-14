95422833dfc9454f95984ceb2bdcbfb6716fd44f
Florian Meinel <florian@scm.io>
2017-11-14 16:06:02 +0100

    log request data on error

--- src/main/scala/com/scalableminds/vkvstore/StoreGrpcImpl.scala
+++ src/main/scala/com/scalableminds/vkvstore/StoreGrpcImpl.scala
@@ -14,9 +14,10 @@ import com.typesafe.scalalogging.LazyLogging
 import scala.concurrent.Future
 
 class StoreGrpcImpl(storeManager: StoreManager) extends StoreGrpc.Store with LazyLogging {
+
   override def get(req: GetRequest) = {
     try {
-      logger.info("received get: " + req.toString.replaceAll("\n"," "))
+      logger.debug("received get: " + req.toString.replaceAll("\n"," "))
       val store = storeManager.getStore(req.collection)
       val versionedKeyValuePairOpt = store.get(req.key, req.version)
       versionedKeyValuePairOpt match {
@@ -27,79 +28,100 @@ class StoreGrpcImpl(storeManager: StoreManager) extends StoreGrpc.Store with Laz
         }
       }
     } catch {
-      case e: Exception => log(e); Future.successful(GetReply(false, Some(e.toString), ByteString.EMPTY, 0))
+      case e: Exception => {
+        log(e, "get", req.toString)
+        Future.successful(GetReply(false, Some(e.toString), ByteString.EMPTY, 0))
+      }
     }
   }
 
   override def put(req: PutRequest) = {
     try {
-      logger.info("received put: " + req.toString.replaceAll("\n"," "))
+      logger.debug("received put: " + req.toString.replaceAll("\n"," "))
       val store = storeManager.getStore(req.collection)
       require(req.version >= 0, "Version numbers must be non-negative")
       store.put(req.key, req.version, req.value.toByteArray)
       Future.successful(PutReply(true))
     } catch {
-      case e: Exception => log(e); Future.successful(PutReply(false, Some(e.toString)))
+      case e: Exception => {
+        log(e, "put", req.toString)
+        Future.successful(PutReply(false, Some(e.toString)))
+      }
     }
   }
 
   override def delete(req: DeleteRequest) = {
     try {
-      logger.info("received delete: " + req.toString.replaceAll("\n"," "))
+      logger.debug("received delete: " + req.toString.replaceAll("\n"," "))
       val store = storeManager.getStore(req.collection)
       store.delete(req.key, req.version)
       Future.successful(DeleteReply(true))
     } catch {
-      case e: Exception => log(e); Future.successful(DeleteReply(false, Some(e.toString)))
+      case e: Exception => {
+        log(e, "delete", req.toString)
+        Future.successful(DeleteReply(false, Some(e.toString)))
+      }
     }
   }
 
   override def getMultipleVersions(req: GetMultipleVersionsRequest) = {
     try {
-      logger.info("received getMultipleVersions: " + req.toString.replaceAll("\n"," "))
+      logger.debug("received getMultipleVersions: " + req.toString.replaceAll("\n"," "))
       val store = storeManager.getStore(req.collection)
       val values = store.getMultipleVersions(req.key, req.oldestVersion, req.newestVersion)
       Future.successful(GetMultipleVersionsReply(true, None, values.map(ByteString.copyFrom(_))))
     } catch {
-      case e: Exception => log(e); Future.successful(GetMultipleVersionsReply(false, Some(e.toString)))
+      case e: Exception => {
+        log(e, "getMultipleVersions", req.toString)
+        Future.successful(GetMultipleVersionsReply(false, Some(e.toString)))
+      }
     }
   }
 
   override def getMultipleKeys(req: GetMultipleKeysRequest) = {
     try {
-      logger.info("received getMultipleKeys: " + req.toString.replaceAll("\n"," "))
+      logger.debug("received getMultipleKeys: " + req.toString.replaceAll("\n"," "))
       val store = storeManager.getStore(req.collection)
       val (keys, values) = store.getMultipleKeys(req.key, req.prefix, req.version)
       Future.successful(GetMultipleKeysReply(true, None, keys, values.map(ByteString.copyFrom(_))))
     } catch {
-      case e: Exception => log(e); Future.successful(GetMultipleKeysReply(false, Some(e.toString)))
+      case e: Exception => {
+        log(e, "getMultipleKeys", req.toString)
+        Future.successful(GetMultipleKeysReply(false, Some(e.toString)))
+      }
     }
   }
 
   override def deleteMultipleVersions(req: DeleteMultipleVersionsRequest) = {
     try {
-      logger.info("received deleteMultipleVersions: " + req.toString.replaceAll("\n", " "))
+      logger.debug("received deleteMultipleVersions: " + req.toString.replaceAll("\n", " "))
       val store = storeManager.getStore(req.collection)
       store.deleteMultipleVersions(req.key, req.oldestVersion, req.newestVersion)
       Future.successful(DeleteMultipleVersionsReply(true))
     } catch {
-      case e: Exception => log(e); Future.successful(DeleteMultipleVersionsReply(false, Some(e.toString)))
+      case e: Exception => {
+        log(e, "deleteMultipleVersions", req.toString)
+        Future.successful(DeleteMultipleVersionsReply(false, Some(e.toString)))
+      }
     }
   }
 
   override def listKeys(req: ListKeysRequest) = {
     try {
-      logger.info("received listKeys: " + req.toString.replaceAll("\n", " "))
+      logger.debug("received listKeys: " + req.toString.replaceAll("\n", " "))
       val store = storeManager.getStore(req.collection)
       val keys = store.listKeys(req.limit, req.offset)
       Future.successful(ListKeysReply(true, None, keys))
     } catch {
-      case e: Exception => log(e); Future.successful(ListKeysReply(false, Some(e.toString)))
+      case e: Exception => {
+        log(e, "listKeys", req.toString)
+        Future.successful(ListKeysReply(false, Some(e.toString)))
+      }
     }
   }
 
   override def backup(req: BackupRequest) = {
-    logger.info("received backup: " + req.toString.replaceAll("\n"," "))
+    logger.debug("received backup: " + req.toString.replaceAll("\n"," "))
     if (storeManager.backupInProgress.compareAndSet(false, true)) {
       try {
         val backupInfoOpt = storeManager.backup
@@ -108,7 +130,10 @@ class StoreGrpcImpl(storeManager: StoreManager) extends StoreGrpc.Store with Laz
           case _ => throw new Exception("Backup did not return valid BackupInfo")
         }
       } catch {
-        case e: Exception => log(e); Future.successful(BackupReply(false, Some("Could not do backup: " + e.toString), 0, 0, 0))
+        case e: Exception => {
+          log(e, "backup", req.toString)
+          Future.successful(BackupReply(false, Some("Could not do backup: " + e.toString), 0, 0, 0))
+        }
       } finally {
         storeManager.backupInProgress.set(false)
       }
@@ -119,19 +144,24 @@ class StoreGrpcImpl(storeManager: StoreManager) extends StoreGrpc.Store with Laz
 
   override def restoreFromBackup(req: RestoreFromBackupRequest) = {
     try {
-      logger.info("received restoreFromBackup: " + req.toString.replaceAll("\n"," "))
+      logger.debug("received restoreFromBackup: " + req.toString.replaceAll("\n"," "))
       storeManager.restoreFromBackup
       Future.successful(RestoreFromBackupReply(true))
     } catch {
-      case e: Exception => log(e); Future.successful(RestoreFromBackupReply(false, Some("Could not restore from backup: " + e.toString)))
+      case e: Exception => {
+        log(e, "restoreFromBackup", req.toString)
+        Future.successful(RestoreFromBackupReply(false, Some("Could not restore from backup: " + e.toString)))
+      }
     }
   }
 
-  private def log(e: Exception) = logger.error(getStackTraceAsString(e))
+  private def log(e: Exception, requestType: String, requestFields: String) = {
+    logger.error(getStackTraceAsString(e) + "\nrequest that caused this error: " + requestType + " (" + requestFields.replaceAll("\n"," ") + ")\n")
+  }
 
   private def getStackTraceAsString(t: Throwable) = {
     val sw = new StringWriter
     t.printStackTrace(new PrintWriter(sw))
-    sw.toString
+    sw.toString.dropRight(1)
   }
 }
